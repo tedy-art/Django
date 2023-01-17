c4354665e5cd5e9bf35dc140d229e2192c2e60ed
Tejas Falke <tejasdf.77@gmail.com>
2023-01-17 23:36:27 +0530

    add modes database

--- adminproject/adminapp/models.py
+++ adminproject/adminapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class student(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     age = models.IntegerField()
     roll = models.IntegerField()
     marks = models.IntegerField()
